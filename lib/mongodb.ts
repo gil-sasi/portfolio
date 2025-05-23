ccd97928db1fc1698502970af75c85857a33b824
Gil Sasi <gilsasiy@gmail.com>
2025-05-23 07:54:21 +0300

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,30 +1,3 @@
-// import mongoose from "mongoose";
-
-// const MONGODB_URI = process.env.MONGODB_URI!;
-
-// if (!MONGODB_URI) {
-//   throw new Error("❌ MONGODB_URI not defined in environment variables");
-// }
-
-// // Cache the connection across hot reloads in dev
-// let cached = (global as any).mongoose;
-
-// if (!cached) {
-//   cached = (global as any).mongoose = { conn: null, promise: null };
-// }
-
-// export async function connectToDatabase() {
-//   if (cached.conn) return cached.conn;
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URI, {
-//       bufferCommands: false,
-//     });
-//   }
-
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
